76e6834b555ed8024605dbdfb684db565893410f
stmaddox <sean.maddox89@yahoo.com>
2022-10-18 02:07:57 -0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+
+
 
 function App() {
   return (
