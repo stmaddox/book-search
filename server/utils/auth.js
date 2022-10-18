b3b54f97638a658f38651b7c021f04fba614e160
stmaddox <sean.maddox89@yahoo.com>
2022-10-17 23:22:34 -0700

    trying to fix .gitignore file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,6 +6,13 @@ const secret = process.env.SECRET;
 const expiration = process.env.EXPIRATION;
 
 module.exports = {
+
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+  
   // function for our authenticated routes
   authMiddleware: function (req, res) {
     // allows token to be sent via  req.query or headers
@@ -30,10 +37,5 @@ module.exports = {
     }
 
     return req;
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
